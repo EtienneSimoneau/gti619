0f13b53c9110083b6d5fe2045150b748ef9efd4a
Etienne Simoneau <etiennesimoneau@hotmail.fr>
2022-04-02 15:54:05 -0400

    Starting point

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,6 @@ Route::resource('client', ClientController::class);
 Route::get('client/{id}/edit', 'ClientController@edit')->name('client.edit');
 Route::put('client/{id}', 'ClientController@update')->name('client.update');
 Route::delete('client/{id}', 'ClientController@destroy')->name('client.destroy');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
